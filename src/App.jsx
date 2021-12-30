aae95aee9426044910bfed61f298e85c0a883917
Prince H Chabveka <pchabveka@my.bcit.ca>
2021-12-29 17:53:45 -0800

    Create skeleton

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+import Header from "./header.jsx"
+import Footer from "./footer.jsx"
+import Note from "./Note.jsx"
+
+function App() {
+    return ( < div > < Header / >
+        <
+        Footer / >
+        <
+        Note / >
+        <
+        /div>)
+    }
+
+    export default App;
---
+++ src/header.jsx
@@ -0,0 +1,9 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+function Header() {
+    return <header > < h1 > Keeper < /h1></header >
+}
+
+
+export default Header;
